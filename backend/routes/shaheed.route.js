8fa9b556d25c5bffe026f87f047f2b0a38c3c23f
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2024-10-20 14:52:26 +0500

    corrected data

--- backend/routes/shaheed.route.js
+++ backend/routes/shaheed.route.js
@@ -11,10 +11,13 @@ const {
   deleteShaheed,
 } = require("../controllers/shaheed.controller");
 
+/// Import middleware for handling file uploads
+const upload = require("../middlewares/shaheed-multer.middleware");
+
 // Define routes and associate them with controller functions
 Router.get("/", getShaheed) // Get all shaheed records
   .get("/:id", getSingleShaheed) // Get a single shaheed record by ID
-  .post("/create", createShaheed) // Create a new shaheed record
+  .post("/create", upload.single("photo"), createShaheed) // Create a new shaheed record
   .patch("/edit/:id", editShaheed) // Update an existing shaheed record
   .delete("/delete/:id", deleteShaheed); // Delete a shaheed record
 
