49da05565f3dd87defa6a8d78a1a56e91583c687
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2024-10-28 16:15:38 +0500

    working

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 // Route setup
 app.use("/api/shaheed", shaheedRoute);
