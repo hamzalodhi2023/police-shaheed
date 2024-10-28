e1de656baa74bbce90fb9f4b5a867d59d689b018
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2024-10-28 22:53:31 +0500

    working

--- backend/app.js
+++ backend/app.js
@@ -20,8 +20,8 @@ const PORT = process.env.PORT;
 // Middleware setup
 app.use(
   cors({
-    // origin: ["https://police-shaheed.vercel.app"],
-    origin: ["http://localhost:5173"],
+    origin: ["https://police-shaheed.vercel.app"],
+    // origin: ["http://localhost:5173"],
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
   })
