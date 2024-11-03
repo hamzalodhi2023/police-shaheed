74d8c6a040ee39da1d8cc2504ed225c2603f29a3
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2024-11-03 22:09:35 +0500

    "Comment out errorElement in App.jsx"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
       {
         path: "/",
         element: <Layout />,
-        errorElement: <Error />,
+        // errorElement: <Error />,
         children: [
           {
             path: "/",
